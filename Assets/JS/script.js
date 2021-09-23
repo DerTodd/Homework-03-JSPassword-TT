09499d2a4205333a823cc3ee6d4695733df5354d
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-09-22 20:57:57 -0500

    Testing new functions

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //Password arrays and variables
-var pwd;
+var pwdLength;
 var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numeric = ["1","2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -9,6 +9,8 @@ var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var selectedcriteria;
 var passW = " ";
 var newpass = " ";
+var newlist = " ";
+
 
 //selectedcriteria = symbols[Math.floor(Math.random() * symbols.length)];
 //console.log(selectedcriteria);
@@ -21,6 +23,75 @@ var newpass = " ";
   //var something = newpass.pop();
   //console.log(something);
 //}
+//Writing the Function to get user input
+function genpass () {
+  var beginp = confirm("Would you like a new password?")
+  console.log(beginp);
+  if (beginp === true) {
+      var alphaU = confirm("Would you like to include upper case letters?");
+      console.log (alphaU);
+  }else {
+    return;
+  };
+  if (alphaU === true) {
+      console.log(alphaU);
+      newlist = newlist.concat(alphaUpper);
+      console.log(newlist);
+  }else {
+        newlist = newlist;
+    }return newlist;
+  };   
+function genpass2 (){
+  var alphaL = confirm("Would you like to include lower case letters?");
+  console.log (alphaL);
+  if (alphaL === true) {
+    console.log(alphaL);
+    newlist = newlist.concat(alphaLower);
+    console.log(newlist);
+}else {
+      newlist = newlist;
+  }return newlist;
+};   
+function genpass3 (){
+  var numch = confirm("Would you like to include numbers?");
+  console.log (numch);
+  if (numch === true) {
+    console.log(numch);
+    newlist = newlist.concat(numeric);
+    console.log(newlist);
+}else {
+      newlist = newlist;
+  }return newlist;
+};   
+function genpass4 (){
+  var symch = confirm("Would you like to include symbols?");
+  console.log (symch);
+  if (symch === true) {
+    console.log(symch);
+    newlist = newlist.concat(symbols);
+    console.log(newlist);
+}else {
+      newlist = newlist;
+  }return newlist;
+}; 
+function howlong (){
+  var lench = prompt("How long would you like your password to be? Please select a number between 8 and 128 (choose 128 if you are insane)");
+  console.log (lench);
+  pwdLength = lench;
+  console.log(pwdLength)
+  return pwdLength;
+}; 
+function funny() {
+  for (var i=0; i < pwdLength; i++) {
+
+    selectedcriteria = symbols[Math.floor(Math.random() * symbols.length)];
+    newpass = newpass.concat(selectedcriteria);
+    console.log(newpass);
+  };
+  return newpass;
+};
+
+//Function designed to return a random string based on iteriations.
 function fun() {
   for (var i=0; i < 5; i++) {
 
@@ -30,9 +101,17 @@ function fun() {
   };
   return newpass;
 };
-fun();
-console.log(passW)
-console.log(newpass)
+genpass()
+genpass2()
+genpass3()
+genpass4()
+howlong()
+
+//fun();
+console.log(passW);
+console.log(newpass);
+console.log(newlist);
+console.log(pwdLength);
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 3rd homework JavaScript: Password Generator
 
 A users wanted a random password generator.  We started by defining what was needed.
-We need to 
+We need to added the variables that at the top of the page and worked on generating a random password using the Math .
